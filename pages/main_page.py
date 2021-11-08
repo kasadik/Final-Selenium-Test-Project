2e561a797641ca3c1811db914e73fa480124232b
Bohdan Krutskevych <lyskav>
2021-11-08 14:02:08 +0200

    Removed constructor from MainPage

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
-from .locators import MainPageLocators
 
 class MainPage(BasePage):
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+    pass
+
