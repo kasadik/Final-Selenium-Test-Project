e83324971caa8cad603f650e00d90620fa10992b
Bohdan Krutskevych <lyskav>
2021-11-09 13:22:46 +0200

    Added Basket tests on Product Page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,3 @@
-import time
-
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from pages.product_page import ProductPage
 from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 import pytest
 
 # @pytest.mark.parametrize('link', [pytest.param(i,marks=pytest.mark.xfail) if i==7 else i for i in range(10)])
@@ -49,6 +50,15 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
     login_page=LoginPage(browser,browser.current_url)
     login_page.should_be_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link='http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/'
+    page=ProductPage(browser,link)
+    page.open()
+    page.go_to_basket()
+    basket=BasketPage(browser,browser.current_url)
+    basket.should_be_empty_basket()
+    basket.should_be_empty_basket_text()
 
 
 
